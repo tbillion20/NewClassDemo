e2f92ea9d3a7783c1dcbd4812c0a2b320ee89612
Travon Billups <travonbillups@yahoo.com>
2017-12-20 23:34:02 -0600

    "Staff" class wass created. Added methods to hold private and public variables for names, hourly rates, and hours worked in Staff class. Methods for accessing private variables created

--- ClassDemo/Program.cs
+++ ClassDemo/Program.cs
@@ -6,6 +6,44 @@ using System.Threading.Tasks;
 
 namespace ClassDemo
 {
+    class Staff
+    {
+        private string nameOfStaff;
+        private const int hourlyRate = 30;
+        private int hWorked;
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+        public void PrintMessage ()
+        {
+            Console.WriteLine("Calculating Pay...");
+        }
+        public int CalculatePay ()
+        {
+            PrintMessage ();
+
+            int staffPay;
+            staffPay = hWorked * hourlyRate;
+
+            if (hWorked > 0)
+                return staffPay;
+            else
+                return 0;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
